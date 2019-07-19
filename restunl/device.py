d911bf2bec1ebaa9d977b408874782cb5c3daf4d
wl327248838 <53027421+wl327248838@users.noreply.github.com>
2019-07-19 15:55:38 +0800

    1

--- restunl/device.py
+++ restunl/device.py
@@ -1,3 +1,6 @@
+from telnetlib import Telnet
+from restunl.helper import *
+
 class Device(object):
     def __init__(self, name):
         self.name = name
@@ -11,17 +14,24 @@ class Device(object):
 
 class Router(Device):
     defaults = {
-        'template': 'iol',
-        'count': 1,
-        'image': 'L3-ADVENTERPRISEK9-M-15.4-1T.bin',
-        'ram': '256',
-        'ethernet': '2',
-        'serial': '0',
-        'type': 'iol',
-        'config': 'unconfigured'
+        "type":"qemu","template":"vios","config":"Unconfigured","delay":0,"icon":"Router.png",
+        "image":"vios-adventerprisek9-m-15.5.3M","name":"Core Router 1","left":"35%","top":"25%",
+        "ram":"1024","console":"telnet","cpu":1,"ethernet":2,"uuid":"641a4800-1b19-427c-ae87-4a8ee90b7790"
     }
 
     def __init__(self, name):
         for key, value in Router.defaults.items():
             setattr(self, key, value)
         super(Router, self).__init__(name)
+        self.url_ip, self.url_port = '', ''
+
+    def set_url(self, url):
+        self.url_ip, self.url_port = str(url).strip('telnet://').split(':')
+        return None
+
+    def send_config(self, config):
+        session = Telnet(self.url_ip, self.url_port)
+        send_and_wait(session, '\r\n')
+        result = send_and_wait(session, config)
+        session.close()
+        return result
--- samples/app-1.py
+++ samples/app-1.py
@@ -1,16 +1,55 @@
 from restunl.unetlab import UnlServer
 from restunl.device import Router
+from restunl.helper import *
 
-LAB_NAME = 'test_1'
+LAB_NAME = 'test1'
+TOPOLOGY = {('R1', 'GigaEthernet0/0'): ('R2', 'GigaEthernet0/0'),
+            ('R2', 'GigaEthernet0/1'): ('R3', 'GigaEthernet0/1'),
+            ('R1', 'GigaEthernet0/1'): ('R3', 'GigaEthernet0/0')}
 
-def app_1():
-    unl = UnlServer('10.14.10.148')
-    unl.login('admin', 'eve')
-    print ("*** CONNECTED TO UNL")
-    lab = unl.create_lab(LAB_NAME)
-    print ("*** CREATED LAB")
-    node_1 = lab.create_node(Router('R1'))
-    print ("*** CREATED NODE")
+
+def app():
+    try:
+        unl = UnlServer('10.14.10.148')
+        unl.login('admin', 'eve')
+        print("*** CONNECTED TO UNL")
+        lab = unl.create_lab(LAB_NAME)
+        lab.cleanup()
+        print("*** CREATED LAB")
+        # Creating topology in UnetLab
+        nodes = dict()
+        for (a_name, a_intf), (b_name, b_intf) in TOPOLOGY.items():
+            # Create a mapping between a Node's name and an object
+            if not a_name in nodes:
+
+                nodes[a_name] = lab.create_node(Router(a_name))
+
+                print("*** NODE {} CREATED".format(a_name))
+
+            if not b_name in nodes:
+                nodes[b_name] = lab.create_node(Router(b_name))
+                print("*** NODE {} CREATED".format(b_name))
+
+            # Extract Node objects using their names and connect them
+            node_a = nodes[a_name]
+            node_b = nodes[b_name]
+            node_a.connect_node(a_intf, node_b, b_intf)
+            print("*** NODES {0} and {1} ARE CONNECTED".format(a_name, b_name))
+        print("*** TOPOLOGY IS BUILT")
+        lab.start_all_nodes()
+        print("*** NODES STARTED")
+        # Reading and pushing configuration
+        for node_name in nodes:
+            conf = read_file('..\\config\\{}.txt'.format(node_name))
+            nodes[node_name].configure(conf)
+            print("*** NODE {} CONFIGURED".format(node_name))
+
+    except Exception as e:
+        print("*** APP FAILED : {}".format(e))
+    finally:
+        print("*** CLEANING UP THE LAB")
+        lab.cleanup()
+        unl.delete_lab(LAB_NAME)
 
 if __name__ == '__main__':
-    app_1()
+    app()
